d9a6bbb28c153bf28a77acaeb5281bb5c720100a
David Yang <32862068+yanglinchao@users.noreply.github.com>
2021-01-02 22:28:25 +0800

    Update Ch4_数组和矢量计算.py

--- Python/PythonForDataAnalysis/Ch4_数组和矢量计算.py
+++ Python/PythonForDataAnalysis/Ch4_数组和矢量计算.py
@@ -105,3 +105,32 @@ data
 
 # 花式索引
 arr = np.empty((8, 4))
+for i in range(8):
+    arr[i]=i
+arr
+# 为了以特定顺序选取行子级，只需传入一个用于指定顺序的整数列表或ndarray
+arr[[4, 3, 0, 6]]
+# 使用负数索引将会从末尾开始选取行
+arr[[-3, -5, -7]]
+# 一次传入多个索引数组返回的是一个一维数组，其中的元素对应各个索引元组
+arr = np.arange(32).reshape((8, 4))
+arr[[1, 5, 7, 2], [0, 3, 1, 2]]
+# 如果想按顺序提取矩形区域，应该按照下述方式
+arr[[1, 5, 7, 2]][:, [0, 3, 1, 2]]
+# 或者使用np.ix_函数，它可以将两个一维整数数组转换为一个用于选取方形区域的索引器
+arr[np.ix_([1, 5, 7, 2], [0, 3, 1, 2])]
+
+# 数组转置和轴对换
+arr = np.arange(15).reshape((3, 5))
+arr
+arr.T
+# 利用np.dot计算矩阵内积
+arr = np.random.randn(6, 3)
+np.dot(arr.T, arr)
+# 高维数组
+arr = np.arange(16).reshape(2, 2, 4)
+arr
+arr.transpose((1, 0, 2))
+# 简单的转置可以使用.T，其实就是进行轴对换，或者使用swapaxes()
+arr
+arr.swapaxes(1, 2)
