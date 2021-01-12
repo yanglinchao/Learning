d9d12d905d273d5461834db218816e0af45a6752
David Yang <32862068+yanglinchao@users.noreply.github.com>
2021-01-13 04:06:09 +0800

    Update Ch7_数据规整化.py

--- Python/PythonForDataAnalysis/Ch7_数据规整化.py
+++ Python/PythonForDataAnalysis/Ch7_数据规整化.py
@@ -104,3 +104,76 @@ another = pd.DataFrame([[7., 8.], [9., 10.], [11., 12.], [16., 17.]],
                        index=['a', 'c', 'e', 'f'], columns=['New York', 'Oregon'])
 left2.join([right2, another])
 left2.join([right2, another], how='outer')
+
+# 轴向连接
+arr = np.arange(12).reshape((3, 4))
+arr
+np.concatenate([arr, arr], axis=1)
+
+s1 = pd.Series([0, 1], index=['a', 'b'])
+s2 = pd.Series([2, 3, 4], index=['c', 'd', 'e'])
+s3 = pd.Series([5, 6], index=['f', 'g'])
+pd.concat([s1, s2, s3])
+pd.concat([s1, s2, s3], axis=1)
+
+s1
+s4 = pd.concat([s1*5, s3])
+s4
+pd.concat([s1, s4], axis=1)
+pd.concat([s1, s4], axis=1, join='inner')
+
+pd.concat([s1, s1, s3],
+          keys=['one', 'two', 'three'])
+pd.concat([s1, s1, s3], axis=1,
+          keys=['one', 'two', 'three'])
+
+df1 = pd.DataFrame(np.arange(6).reshape(3, 2), index=['a', 'b', 'c'], columns=['one', 'two'])
+df2 = pd.DataFrame(5+np.arange(4).reshape(2, 2), index=['a', 'c'], columns=['three', 'four'])
+pd.concat([df1, df2])
+pd.concat([df1, df2], axis=1)
+pd.concat([df1, df2], axis=1, keys=['level1', 'level2'])
+pd.concat({'level1':df1, 'level2':df2}, axis=1)
+pd.concat([df1, df2], axis=1, keys=['level1', 'level2'], names=['upper', 'lower'])
+
+df1 = pd.DataFrame(np.random.randn(3, 4), columns=['a', 'b', 'c', 'd'])
+df2 = pd.DataFrame(np.random.randn(2, 3), columns=['b', 'd', 'a'])
+pd.concat([df1, df2], ignore_index=True)
+
+# 合并重叠数据
+a = pd.Series([np.nan, 2.5, np.nan, 3.5, 4.5, np.nan], index=['f', 'e', 'd', 'c', 'b', 'a'])
+b = pd.Series(np.arange(len(a), dtype=np.float64), index=['f', 'e', 'd', 'c', 'b', 'a'])
+b[-1] = np.nan
+np.where(pd.isnull(a), b, a)
+b[:-2].combine_first(a[2:])
+
+df1 = pd.DataFrame({'a':[1., np.nan, 5., np.nan],
+                    'b':[np.nan, 2., np.nan, 6.],
+                    'c':range(2, 18, 4)})
+df2 = pd.DataFrame({'a':[5., 4., np.nan, 3., 7.],
+                    'b':[np.nan, 3., 4., 6., 8.]})
+df1
+df2
+df1.combine_first(df2)
+
+# 重塑层次化索引
+data = pd.DataFrame(np.arange(6).reshape((2, 3)),
+                    index=pd.Index(['Ohio', 'Colorado'], name='state'),
+                    columns=pd.Index(['one', 'two', 'three'], name='number'))
+data
+result = data.stack()
+result
+result.unstack()
+result.unstack(0)
+result.unstack('state')
+
+s1 = pd.Series([0, 1, 2, 3], index=['a', 'b', 'c', 'd'])
+s2 = pd.Series([4, 5, 6], index=['c', 'd', 'e'])
+data2 = pd.concat([s1, s2], keys=['one', 'two'])
+data2.unstack()
+data2.unstack().stack()
+data2.unstack().stack(dropna=False)
+df = pd.DataFrame({'left':result, 'right':result+5},
+                  columns=pd.Index(['left', 'right'], name='side'))
+df
+df.unstack('state')
+df.unstack('state').stack('side')
