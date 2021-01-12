1e5e28c80eb3bec95fc32666d70f64ac384321b6
David Yang <32862068+yanglinchao@users.noreply.github.com>
2021-01-12 12:36:36 +0800

    Update Ch7_数据规整化.py

--- Python/PythonForDataAnalysis/Ch7_数据规整化.py
+++ Python/PythonForDataAnalysis/Ch7_数据规整化.py
@@ -64,3 +64,43 @@ pd.merge(left1, right1,
          left_on='key',
          right_index=True,
          how='outer')
+
+# 层次化索引的合并
+lefth = pd.DataFrame({'key1':['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'],
+                      'key2':[2000, 2001, 2002, 2001, 2002],
+                      'data':np.arange(5.)})
+righth = pd.DataFrame(np.arange(12).reshape((6, 2)),
+                      index=[['Nevada', 'Nevada', 'Ohio', 'Ohio', 'Ohio', 'Ohio'],
+                             [2001, 2000, 2000, 2000, 2001, 2002]],
+                      columns=['event1', 'event2'])
+lefth
+righth
+pd.merge(lefth, righth, left_on=['key1', 'key2'], right_index=True)
+pd.merge(lefth, righth, left_on=['key1', 'key2'], right_index=True, how='outer')
+
+# 合并双方的索引
+left2 = pd.DataFrame([[1., 2.], [3., 4.], [5., 6.]], index=['a', 'c', 'e'],
+                     columns=['Ohio', 'Nevada'])
+right2 = pd.DataFrame([[7., 8.], [9., 10.], [11., 12.], [13., 14.]],
+                      index=['b', 'c', 'd', 'e'], columns=['Missouri', 'Alabama'])
+left2
+right2
+pd.merge(left2, right2, how='outer', left_index=True, right_index=True)
+
+# join方法
+left2 = pd.DataFrame([[1., 2.], [3., 4.], [5., 6.]], index=['a', 'c', 'e'],
+                     columns=['Ohio', 'Nevada'])
+right2 = pd.DataFrame([[7., 8.], [9., 10.], [11., 12.], [13., 14.]],
+                      index=['b', 'c', 'd', 'e'], columns=['Missouri', 'Alabama'])
+left2.join(right2)
+left2.join(right2, how='outer')
+
+left1 = pd.DataFrame({'key':['a', 'b', 'a', 'a', 'b', 'c'], 'value':range(6)})
+right1 = pd.DataFrame({'group_val':[3.5, 7]}, index=['a', 'b'])
+left1.join(right1, on='key')
+
+# 传入DataFrame
+another = pd.DataFrame([[7., 8.], [9., 10.], [11., 12.], [16., 17.]],
+                       index=['a', 'c', 'e', 'f'], columns=['New York', 'Oregon'])
+left2.join([right2, another])
+left2.join([right2, another], how='outer')
