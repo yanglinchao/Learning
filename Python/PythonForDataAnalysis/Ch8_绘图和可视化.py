2aa18b5ab9a5f22d70cce5585b5f3dff934bed2b
unknown <xinx_yang@163.com>
2021-01-21 00:11:56 +0800

    Update Ch8_绘图和可视化.py

--- Python/PythonForDataAnalysis/Ch8_绘图和可视化.py
+++ Python/PythonForDataAnalysis/Ch8_绘图和可视化.py
@@ -36,3 +36,67 @@ data = np.random.randn(30).cumsum()
 plt.plot(data, 'k--', label='Default')
 plt.plot(data, 'k--', drawstyle='steps-post', label='steps-post')
 plt.legend(loc='best')
+
+# 刻度、标签和图例
+fig = plt.figure(); ax = fig.add_subplot(1, 1, 1)
+ax.plot(np.random.randn(1000).cumsum())
+
+ticks = ax.set_xticks([0, 250, 500, 750, 1000])
+labels = ax.set_xticklabels(['one', 'two', 'three', 'four', 'five'],
+                            rotation=30, fontsize='small')
+
+ax.set_title('My first matplotlib plot')
+ax.set_xlabel('Stages')
+
+# 添加图例
+fig = plt.figure(); ax = fig.add_subplot(1, 1, 1)
+ax.plot(np.random.randn(1000).cumsum(), 'k', label='one')
+
+fig = plt.figure(); ax = fig.add_subplot(1, 1, 1)
+ax.plot(np.random.randn(1000).cumsum(), 'k', label='two')
+
+fig = plt.figure(); ax = fig.add_subplot(1, 1, 1)
+ax.plot(np.random.randn(1000).cumsum(), 'k', label='three')
+
+ax.legend(loc='best')
+
+# 注解以及在Subplot上绘图
+from datetime import datetime
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+data = pd.read_csv('pydata-book\examples\spx.csv', index_col=0, parse_dates=True)
+spx = data['SPX']
+
+spx.plot(ax=ax, style='k-')
+
+crisis_data = [
+    (datetime(2007, 10, 11), 'Peak of bull market'),
+    (datetime(2008, 3, 12), 'Bear Stearns Fails'),
+    (datetime(2008, 9, 15), 'Lehman Bankruptcy')
+    ]
+
+for date, label in crisis_data:
+    ax.annotate(label, xy=(date, spx.asof(date)+50),
+                xytext=(date, spx.asof(date)+200),
+                arrowprops=dict(facecolor='black'),
+                horizontalalignment='left',
+                verticalalignment='top')
+
+# 放大到2007-2010
+ax.set_xlim(['1/1/2007', '1/1/2011'])
+ax.set_ylim([600, 1800])
+
+ax.set_title('Important dates in 2008-2009 financial crisis')
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+rect = plt.Rectangle((0.2, 0.75), 0.4, 0.15, color='k', alpha=0.3)
+circ = plt.Circle((0.7, 0.2), 0.15, color='b', alpha=0.3)
+pgon = plt.Polygon([[0.15, 0.15], [0.35, 0.4], [0.2, 0.6]], color='g', alpha=0.5)
+
+ax.add_patch(rect)
+ax.add_patch(circ)
+ax.add_patch(pgon)
