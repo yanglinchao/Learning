976efe69c78c7404b75719fb6fb956dc818a839f
unknown <xinx_yang@163.com>
2021-01-25 02:04:57 +0800

    Update Ch8_绘图和可视化.py

--- Python/PythonForDataAnalysis/Ch8_绘图和可视化.py
+++ Python/PythonForDataAnalysis/Ch8_绘图和可视化.py
@@ -147,3 +147,72 @@ comp2 = np.random.normal(10, 2, size=200) # N(10, 4)
 values = pd.Series(np.concatenate([comp1, comp2]))
 values.hist(bins=100, alpha=0.3, color='k', density=True)
 values.plot(kind='kde', style='k--')
+
+# 散布图
+macro = pd.read_csv('pydata-book/examples/macrodata.csv')
+data = macro[['cpi', 'm1', 'tbilrate', 'unemp']]
+trans_data = np.log(data).diff().dropna()
+trans_data[-5:]
+plt.scatter(trans_data['m1'], trans_data['unemp'])
+plt.title('Changes in log %s vs. log %s' % ('m1', 'unemp'))
+
+pd.plotting.scatter_matrix(trans_data, diagonal='kde', color='k', alpha=0.3)
+
+
+# 绘制地图
+data = pd.read_csv('pydata-book/datasets/haiti/Haiti.csv')
+data[['INCIDENT DATE', 'LATITUDE', 'LONGITUDE']][:10]
+data['CATEGORY'][:6]
+data.describe()
+data = data[(data.LATITUDE>18)&(data.LATITUDE<20)&
+            (data.LONGITUDE>-75)&(data.LONGITUDE<-70)&
+            data.CATEGORY.notnull()]
+def to_cat_list(catstr):
+    stripped = (x.strip() for x in catstr.split(','))
+    return [x for x in stripped if x]
+def get_all_catefories(cat_series):
+    cat_sets = (set(to_cat_list(x)) for x in cat_series)
+    return sorted(set.union(*cat_sets))
+def get_english(cat):
+    code, names = cat.split('.')
+    if '|' in names:
+        names = names.split('|')[1]
+    return code, names.strip()
+get_english('2.Urgences logistiques | Vital Lines')
+
+all_cats = get_all_catefories(data.CATEGORY)
+# 生成器表达式
+english_mapping = dict(get_english(x) for x in all_cats)
+english_mapping['2a']
+
+def get_code(seq):
+    return [x.split('.')[0] for x in seq if x]
+all_codes = get_code(all_cats)
+code_index = pd.Index(np.unique(all_codes))
+dummy_frame = pd.DataFrame(np.zeros((len(data), len(code_index))), index=data.index,
+                           columns=code_index)
+dummy_frame.iloc[:, :6]
+
+for row, cat in zip(data.index, data.CATEGORY):
+    codes = get_code(to_cat_list(cat))
+    dummy_frame.loc[row, codes] = 1
+data = data.join(dummy_frame.add_prefix('category_'))
+data.iloc[:, 10:15]
+
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+
+def basic_haiti_map(ax=None, lllat=17.25, urlat=20.25,
+                    lllon=-75, urlon=-71):
+    # 创建极球面投影的Basemap实例。
+    m = Basemap(ax=ax, projection='stere',
+                lon_0=(urlon+lllon)/2,
+                lat_0=(urlat+lllat)/2,
+                llcrnrlat=lllat, urcrnrlat=urlat,
+                llcrnrlon=lllon, urcrnrlon=urlon,
+                resolution='f')
+    # 绘制海岸线、洲界、国界以及地图边界
+    m.drawcoastlines()
+    m.drawstates()
+    m.drawcountries()
+    return m
