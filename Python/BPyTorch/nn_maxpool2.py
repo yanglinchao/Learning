6035619e17027b2549b37aefc0c5db20409d832e
yanglinchao <xinx_yang@163.com>
2022-07-12 23:23:50 +0800

    Create nn_maxpool2.py

---
+++ Python/BPyTorch/nn_maxpool2.py
@@ -0,0 +1,32 @@
+import torch
+import torchvision.datasets
+from torch import nn
+from torch.nn import MaxPool2d
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+dataset = torchvision.datasets.CIFAR10("D:/Data/CIFAR10", train=False, download=True, transform=torchvision.transforms.ToTensor())
+dataloader = DataLoader(dataset, batch_size=64)
+
+class Tudui(nn.Module):
+    def __init__(self):
+        super(Tudui, self).__init__()
+        self.maxpool1 = MaxPool2d(kernel_size=3, ceil_mode=True)
+
+    def forward(self, input):
+        output = self.maxpool1(input)
+        return output
+
+tudui = Tudui()
+
+writer = SummaryWriter("C:/Users/ylc/GitHub/Learning/Python/BPyTorch/logs_maxpool")
+step = 0
+
+for data in dataloader:
+    imgs, targets = data
+    writer.add_images("input", imgs, step)
+    output = tudui(imgs)
+    writer.add_images("output", output, step)
+    step = step + 1
+
+writer.close()
